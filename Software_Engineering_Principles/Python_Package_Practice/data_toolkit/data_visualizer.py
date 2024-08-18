28b68edb90904f0da79865239a534b9bcf4a421d
MahbubHossainFaisal <mahbubhossain249@gmail.com>
2024-08-19 00:56:10 +0600

    updated

--- Software_Engineering_Principles/Python_Package_Practice/data_toolkit/data_visualizer.py
+++ Software_Engineering_Principles/Python_Package_Practice/data_toolkit/data_visualizer.py
@@ -29,3 +29,44 @@ def plot_line_chart(data,x_column,y_column,title=None,x_label=None,y_label=None,
     
     plt.show()
 
+def plot_histogram(data, column, title=None, xlabel=None, ylabel=None, bins=10, color='blue', save_path=None):
+    plt.figure(figsize=(10, 6))
+    plt.hist(data[column], bins=bins, color=color)
+    if title:
+        plt.title(title)
+    if xlabel:
+        plt.xlabel(xlabel)
+    if ylabel:
+        plt.ylabel(ylabel)
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+def plot_scatter_plot(data, x_column, y_column, title=None, xlabel=None, ylabel=None, color='blue', save_path=None):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(data[x_column], data[y_column], color=color)
+    if title:
+        plt.title(title)
+    if xlabel:
+        plt.xlabel(xlabel)
+    if ylabel:
+        plt.ylabel(ylabel)
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+
+def plot_heatmap(data, title=None, xlabel=None, ylabel=None, cmap='viridis', save_path=None):
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(data.corr(), annot=True, fmt=".2f", cmap=cmap)
+    if title:
+        plt.title(title)
+    if xlabel:
+        plt.xlabel(xlabel)
+    if ylabel:
+        plt.ylabel(ylabel)
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+
