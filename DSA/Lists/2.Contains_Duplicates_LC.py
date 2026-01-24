73d8baf818b34df787d9eb7a0e9d043cabae38f0
MahbubHossainFaisal <mahbubhossain249@gmail.com>
2026-01-24 16:16:01 +0600

    Contains duplicates list LC

---
+++ DSA/Lists/2.Contains_Duplicates_LC.py
@@ -0,0 +1,30 @@
+# Brute force
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+
+# Optimal
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+
+
+# Best
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
