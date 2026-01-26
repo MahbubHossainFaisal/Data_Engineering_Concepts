842120f009ccf3a077401150bd4254137dccd129
MahbubHossainFaisal <mahbubhossain249@gmail.com>
2026-01-27 01:20:35 +0600

    Valid anagram 242 LC

---
+++ DSA/Lists/4.Valid_Anagram_LC_242.py
@@ -0,0 +1,32 @@
+# Solution 1
+# Time Complexity: O(n log n)
+# However, O(n) + O(n) + O(n) = O(3n) = O(n)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+       sorted_s = ''.join(sorted(s))
+       sorted_t = ''.join(sorted(t))
+       return sorted_s==sorted_t
+    
+
+# Solutio 2
+# Time Complexity: O(n)
+# Space Complexity: O(1) - Since the size of the hash map will be at most 26 for lowercase letters
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        library = {}
+
+        for i in range(len(s)):
+            library[s[i]] = library.get(s[i],0)+1
+
+        for i in range(len(t)):
+            if t[i] in library:
+                library[t[i]]-=1
+            else:
+                return False
+        for key,value in library.items():
+            if value!=0:
+                return False
+            
+        return True
