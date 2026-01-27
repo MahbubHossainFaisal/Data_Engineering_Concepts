69ab5b3619f9a23a234a3bcaab4b3705fcdfd4c8
MahbubHossainFaisal <mahbubhossain249@gmail.com>
2026-01-28 00:26:39 +0600

    Valid anagram 242 LC updated

--- DSA/Lists/4.Valid_Anagram_LC_242.py
+++ DSA/Lists/4.Valid_Anagram_LC_242.py
@@ -1,4 +1,4 @@
-# Solution 1
+# Solution 1 using sorting
 # Time Complexity: O(n log n)
 # However, O(n) + O(n) + O(n) = O(3n) = O(n)
 class Solution:
@@ -8,7 +8,7 @@ class Solution:
        return sorted_s==sorted_t
     
 
-# Solutio 2
+# Solutio 2 using dictionary
 # Time Complexity: O(n)
 # Space Complexity: O(1) - Since the size of the hash map will be at most 26 for lowercase letters
 class Solution:
@@ -30,3 +30,22 @@ class Solution:
                 return False
             
         return True
+    
+
+# Solution 3 using list
+# Time Complexity: O(n) 
+# Space Complexity: O(1) - Since the size of the list will be at most 26 for lowercase letters
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        library = [0] * 26 
+
+        for i in range(len(s)):
+            library[ord(s[i])-ord('a')] +=1
+            library[ord(t[i])-ord('a')] -=1
+
+        for i in library:
+            if i!=0:
+                return False
+        return True 
